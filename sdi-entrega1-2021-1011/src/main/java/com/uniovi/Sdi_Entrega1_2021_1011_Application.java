e1c5e2307d72a57322ef046d599b428a2a78c345
uo270824 <uo270824@uniovi.es>
2021-03-15 14:03:56 +0100

    Creación del proyecto Spring boot base

---
+++ sdi-entrega1-2021-1011/src/main/java/com/uniovi/Sdi_Entrega1_2021_1011_Application.java
@@ -0,0 +1,13 @@
+package com.uniovi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi_Entrega1_2021_1011_Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sdi_Entrega1_2021_1011_Application.class, args);
+	}
+
+}
---
+++ sdi-entrega1-2021-1011/src/test/java/com/uniovi/Sdi_Entrega1_2021_1011_ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniovi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdi_Entrega1_2021_1011_ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
